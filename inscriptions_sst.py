da428e64ab330cc2292bd9e0dc911b227860a1b3
Émile Jetzer <emile.jetzer@polymtl.ca>
2022-09-08 18:15:20 -0400

    Séparation des langues et retrait de l'index

--- inscriptions_sst.py
+++ inscriptions_sst.py
@@ -16,11 +16,11 @@ from polygphys.outils.reseau.msforms import MSFormConfig, MSForm
 from polygphys.outils.reseau.courriel import Courriel
 
 
-
 class SSTSIMDUTInscriptionConfig(MSFormConfig):
 
     def default(self):
         return (Path(__file__).parent / 'inscription.cfg').open().read()
+
 
 class SSTSIMDUTInscriptionForm(MSForm):
 
@@ -39,19 +39,20 @@ class SSTSIMDUTInscriptionForm(MSForm):
             html = f'<p>{message}</p>'
 
             if not cadre.empty:
-                fichier_temp = Path('nouvelles_entrées.xlsx')
-                with pd.ExcelWriter(str(fichier_temp)) as excel_temp:
-                    français = cadre.loc[cadre.Langue == 'Français', :]
-                    english = cadre.loc[cadre.Langue == 'English', :]
-                    français.to_excel(excel_temp, 'Français')
-                    english.to_excel(excel_temp, 'English')
-                pièces_jointes.append(fichier_temp)
+                français = cadre.loc[cadre.Langue == 'Français', :]
+                english = cadre.loc[cadre.Langue == 'English', :]
 
-                #destinataire = destinataire\
+                français.to_excel('simdut_français.xlsx', index=False)
+                english.to_excel('simdut_english.xlsx', index=False)
+
+                pièces_jointes.append('simdut_français.xlsx')
+                pièces_jointes.append('simdut_english.xlsx')
+
+                # destinataire = destinataire\
                 #   + ','\
                 #       + self.config.get('courriel', 'superviseur')
                 message = 'Bonjour! Voici les nouvelles inscriptions à faire pour le SIMDUT. Bonne journée!'
-                html = f'<p>{message}</p>{cadre.to_html()}'
+                html = f'<p>{message}</p><hr/>{français.to_html(index=False)}<hr/>{english.to_html(index=False)}'
         except Exception as e:
             message = f'L\'erreur {e} s\'est produite.'
             html = f'<p>{message}</p>'
@@ -64,29 +65,22 @@ class SSTSIMDUTInscriptionForm(MSForm):
                             pièces_jointes=pièces_jointes)
         courriel.envoyer(self.config.get('courriel', 'serveur'))
 
+
 def main():
-        chemin_config = Path('~').expanduser() / 'simdut.cfg'
-        config = SSTSIMDUTInscriptionConfig(chemin_config)
+    chemin_config = Path('~').expanduser() / 'simdut.cfg'
+    config = SSTSIMDUTInscriptionConfig(chemin_config)
 
-        dossier = OneDrive('',
-                           config.get('onedrive', 'organisation'),
-                           config.get('onedrive', 'sous-dossier'),
-                           partagé=True)
-        fichier = dossier / config.get('formulaire', 'nom')
-        config.set('formulaire', 'chemin', str(fichier))
+    dossier = OneDrive('',
+                       config.get('onedrive', 'organisation'),
+                       config.get('onedrive', 'sous-dossier'),
+                       partagé=True)
+    fichier = dossier / config.get('formulaire', 'nom')
+    config.set('formulaire', 'chemin', str(fichier))
 
-        formulaire = SSTSIMDUTInscriptionForm(config)
+    formulaire = SSTSIMDUTInscriptionForm(config)
 
-        schedule.every().friday.at('13:00').do(formulaire.mise_à_jour)
+    formulaire.mise_à_jour()
 
-        formulaire.mise_à_jour()
-        try:
-            print('On commence...')
-            while True:
-                schedule.run_pending()
-                time.sleep(1)
-        except KeyboardInterrupt:
-            print('Fin.')
 
 if __name__ == '__main__':
     main()
